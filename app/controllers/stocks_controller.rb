88161d810b4bf64ae666e39b15ad5a52e5caa783
Gabriel Castro <gabocastrosemper28@gmail.com>
2025-01-29 05:57:47 -0500

    added table in stock index

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,5 +1,7 @@
 class StocksController < ApplicationController
   def index
+    @tracked_stocks = current_user.stocks if user_signed_in?
+    
     if params[:stock].present?
       symbol = params[:stock].upcase
       fmp = FmpApi.new
@@ -9,9 +11,9 @@ class StocksController < ApplicationController
         flash[:alert] = "No data available for this symbol."
         @quote = nil
       end
+
     else
       flash[:alert] = "Please enter a stock symbol"
-      render :index
     end
   end
 end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,6 @@
 class Stock < ApplicationRecord
+    has_many :user_stocks
+    has_many :users, through: :user_stocks
+
+    validates :name, :ticker, presence: true
 end
